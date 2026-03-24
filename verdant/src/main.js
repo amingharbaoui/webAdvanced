287d70722208e94236c08a7b6c57ace72f6e0f48
Amin <132141736+amingharbaoui@users.noreply.github.com>
2026-03-24 11:09:43 +0100

    changed new classes name + improved contrast color and cleaning files

--- verdant/src/main.js
+++ verdant/src/main.js
@@ -1,16 +1,16 @@
 
 let body = document.body;
-let themeButton = document.querySelector(".theme_btn");
+let themeButton = document.querySelector(".navbar_container_buttons_theme");
 
 const isDark = localStorage.getItem("darkMode") === "true";
-body.classList.toggle("dark_body", isDark);
+body.classList.toggle("dark_container_body", isDark);
 
-let moonIcon = document.querySelector(".moon");
-let sunIcon = document.querySelector(".sunny");
+let moonIcon = document.querySelector(".navbar_container_buttons_theme_moon");
+let sunIcon = document.querySelector(".navbar_container_buttons_theme_sunny");
 
 // Dark mode toggle
 themeButton.addEventListener("click", () => {
-    const isDark = body.classList.toggle("dark_body");
+    const isDark = body.classList.toggle("dark_container_body");
     sunIcon.style.display = isDark ? "block" : "none";
     moonIcon.style.display = isDark ? "none" : "block";
     localStorage.setItem("darkMode", isDark);
