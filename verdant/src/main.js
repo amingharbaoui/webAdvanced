878e112363763875aefc69bd0153ab59bbba9d22
Amin <132141736+amingharbaoui@users.noreply.github.com>
2026-04-07 16:20:52 +0200

    added scroll sentinel to dynamic add elements on scroll + added filter for card that are not display original image

--- verdant/src/main.js
+++ verdant/src/main.js
@@ -10,6 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let moonIcon = document.querySelector(".navbar_container_buttons_theme_moon");
     let sunIcon = document.querySelector(".navbar_container_buttons_theme_sunny");
 
+    let currentPage = 1;
+    let lastPage = Infinity;
+    let isLoading = false;
+
 // Dark mode toggle
 
     function toggleDarkMode() {
@@ -24,15 +28,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Load plants Function
 
-    async function loadPlants() {
+    async function loadPlants(page = 1) {
+        if(isLoading) {
+            return;
+        }
+        isLoading = true;
+
         const gallery = document.querySelector("#card_container_items_gallery");
         const template = document.querySelector("#plant_card_template");
 
         try {
-            const data = await getPlantsList(1);
-            gallery.innerHTML = "";
+            const data = await getPlantsList(page);
 
-            data.data.forEach((plant) => {
+            currentPage = data.current_page;
+            lastPage = data.last_page;
+
+            data.data.filter((plant) => plant.default_image?.original_url).forEach((plant) => {
                 const clone = template.content.cloneNode(true);
                 const imageBox = clone.querySelector(".card_container_item_content_intern_image");
                 const title = clone.querySelector(".card_container_items_item_content_extern_information_title");
@@ -49,7 +60,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
         }catch (error) {
             console.error("Error: " + error);
+        }finally {
+            isLoading = false;
         }
     }
-    loadPlants()
+
+    const sentinel = document.querySelector("#scroll_sentinel");
+    const observer = new IntersectionObserver((entries) => {
+        const entry = entries[0];
+
+        if(entry.isIntersecting && !isLoading && currentPage < lastPage) {
+            loadPlants(currentPage + 1);
+        }
+    }, {
+        root: null,
+        rootMargin: "0px 0px 300px 0px",
+        threshold: 0.1,
+    })
+    observer.observe(sentinel);
+    loadPlants(1);
 })
