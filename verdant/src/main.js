0fd642e2c7b8b8a5ae69a96963ccc828e1ae1a4a
Amin <132141736+amingharbaoui@users.noreply.github.com>
2026-04-07 16:01:19 +0200

    added new logic for javascript + added mansory design layout to gallery

--- verdant/src/main.js
+++ verdant/src/main.js
@@ -24,26 +24,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Load plants Function
 
-
     async function loadPlants() {
-        const cards = document.querySelectorAll("#card_container_items_gallery");
+        const gallery = document.querySelector("#card_container_items_gallery");
+        const template = document.querySelector("#plant_card_template");
 
         try {
             const data = await getPlantsList(1);
-            data.data.slice(0, cards.length).forEach((plant, index) => {
-                const card = cards[index];
+            gallery.innerHTML = "";
 
-                card.querySelector(".card_container_item_content_intern_image").innerHTML =
-                    `<img class="card_container_item_content_intern_image_img" src="${plant.default_image?.thumbnail || ""}" alt="${plant.common_name}">`;
+            data.data.forEach((plant) => {
+                const clone = template.content.cloneNode(true);
+                const imageBox = clone.querySelector(".card_container_item_content_intern_image");
+                const title = clone.querySelector(".card_container_items_item_content_extern_information_title");
+                const subtitle = clone.querySelector(".card_container_items_item_content_extern_information_subtitle");
 
-                card.querySelector(".card_container_items_item_content_extern_information_title").textContent = plant.common_name;
-                card.querySelector(".card_container_items_item_content_extern_information_subtitle").textContent = plant.scientific_name?.join(", ");
+                imageBox.innerHTML = `<img class="card_container_item_content_intern_image_img" src="${plant.default_image?.original_url || ""}" alt="${plant.common_name}"> `;
+
+                title.textContent = plant.common_name;
+                subtitle.textContent = plant.scientific_name?.[0];
+
+                gallery.appendChild(clone);
             });
+
+
         }catch (error) {
             console.error("Error: " + error);
         }
     }
-
-    loadPlants();
-
-});
+    loadPlants()
+})
